21251f514e649843e03ebd1782a80452e0237864
CESAR <CESAR@hp>
2016-06-07 10:54:53 -0500

    conexionbd

--- Web Pages/index.php
+++ Web Pages/index.php
@@ -12,6 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
+        echo "<h1>Zannier dice que no sirve tu huevada</h1>"
         ?>
     </body>
 </html>
