be45c7a8715a8686d1fcb6190b6de22c722e1365
CESAR <CESAR@hp>
2016-06-09 12:41:59 -0500

    creating dao step 2

--- dao/AerolineaDAO.php
+++ dao/AerolineaDAO.php
@@ -14,15 +14,77 @@
 class AerolineaDAO {
 
     //put your code here
-    function Ingresar($id,$nombre) {
+    function create($id,$nombre) {
         include "../config/Conexion.php";
-        $sql = "insert into aerolinea(AER_ID,AER_NOM)values( '".$id."','".$nombre."')";
+        $sql = "INSERT INTO aerolinea(AER_ID,AER_NOM)VALUES( '".$id."','".$nombre."')";
         if ($link->query($sql) === TRUE) {
             echo "New record created successfully";
         } else {
             echo "Error: " . $sql . "<br>" . $link->error;
         }
 
+        $link->close();
+    }
+
+    function readall() {
+        include '../config/Conexion.php';
+        $sql = "SELECT * FROM aerolinea order by AER_ID";
+        $result = $link->query($sql);
+        echo "<table border = '1'> \n";
+        echo "<tr><td>Nombre</td><td>E-Mail</td></tr> \n";
+        while ($reg = mysqli_fetch_array($result)) {
+            echo "<tr><td>$reg[0]</td><td>$reg[1]</td></tr> \n";
+        }
+        $link->close();
+    }
+    
+    function delete($id) {
+        include "../config/Conexion.php";
+        $sql = "DELETE FROM aerolinea"
+                . " WHERE AER_ID='".$id."'";
+        if ($link->query($sql) === TRUE) {
+            echo "Record deleted successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $link->error;
+        }
+
+        $link->close();
+    }
+    function update($id,$nombre){
+        include "../config/Conexion.php";
+        $sql = "UPDATE aerolinea"
+                . " SET AER_NOM='".$nombre."'"
+                . " WHERE AER_ID='".$id."'";
+        if ($link->query($sql) === TRUE) {
+            echo "Record updated successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $link->error;
+        }
+
+        $link->close();
+    }
+    function searchById($id) {
+        include '../config/Conexion.php';
+        $sql = "SELECT * FROM aerolinea order by AER_ID "
+                . "WHERE AER_ID='".$id."'";
+        $result = $link->query($sql);
+        echo "<table border = '1'> \n";
+        echo "<tr><td>Nombre</td><td>E-Mail</td></tr> \n";
+        while ($reg = mysqli_fetch_array($result)) {
+            echo "<tr><td>$reg[0]</td><td>$reg[1]</td></tr> \n";
+        }
+        $link->close();
+    }
+    function SearchByName($nombre) {
+        include '../config/Conexion.php';
+        $sql = "SELECT * FROM aerolinea order by AER_ID "
+                . "WHERE AER_ID='".$nombre."'";
+        $result = $link->query($sql);
+        echo "<table border = '1'> \n";
+        echo "<tr><td>Nombre</td><td>E-Mail</td></tr> \n";
+        while ($reg = mysqli_fetch_array($result)) {
+            echo "<tr><td>$reg[0]</td><td>$reg[1]</td></tr> \n";
+        }
         $link->close();
     }
 }
